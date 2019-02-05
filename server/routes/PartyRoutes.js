08dc4f37f5479c1e1fefeed7683955b6c24a44d3
Eneh <erozonachi.james@gmail.com>
2019-02-05 16:44:15 +0100

    chore(refactor): refactor controllers, models, and routes
    - ensure controllers' methods are static
    - ensure read identifiers are changed to get identifier
    - ensure routes reference the controllers' static methods
    - ensure variable are unwraped from string object in models
    
    [Delivers #163729008]

--- server/routes/PartyRoutes.js
+++ server/routes/PartyRoutes.js
@@ -9,20 +9,20 @@ import * as Constants from '../helpers/Constants';
 import PartyValidator from '../helpers/validationlib/PartyValidator';
 import PartyController from '../controllers/PartyController.js';
 
-export default function entryRoutes(app) {
-  const party = new PartyController();
+export default function partyRoutes(app) {
+
   // POST /api/v1/parties
-  app.post(`${Constants.API_BASE_URL}/parties`, PartyValidator.create, party.create);
+  app.post(`${Constants.API_BASE_URL}/parties`, PartyValidator.create, PartyController.create);
 
   // PATCH /api/v1/parties
-  app.patch(`${Constants.API_BASE_URL}/parties`,  PartyValidator.update, party.update);
+  app.patch(`${Constants.API_BASE_URL}/parties`,  PartyValidator.update, PartyController.update);
 
   // GET /api/v1/parties
-  app.get(`${Constants.API_BASE_URL}/parties`, party.read);
+  app.get(`${Constants.API_BASE_URL}/parties`, PartyController.getParty);
 
   // GET api/v1/parties/:id
-  app.get(`${Constants.API_BASE_URL}/parties/:id`, PartyValidator.checkID, party.readById);
+  app.get(`${Constants.API_BASE_URL}/parties/:id`, PartyValidator.checkID, PartyController.getPartyById);
 
   // DELETE api/v1/parties/:id
-  app.delete(`${Constants.API_BASE_URL}/parties/:id`, PartyValidator.checkID, party.delete);
+  app.delete(`${Constants.API_BASE_URL}/parties/:id`, PartyValidator.checkID, PartyController.delete);
 }
