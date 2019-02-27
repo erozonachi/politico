9a6ac9378a24be4809983083bdf46c282d6b1bac
Eneh <erozonachi.james@gmail.com>
2019-02-27 13:40:33 +0100

    chore(dashboard): modify party integration
    - ensure party is modified via the modify party endpoint on successful
      client-side validation of the modify party input form
    
    [Delivers: #164223191]

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json({ extended: true }));
 app.use((req, res, next) => { // https://enable-cors.org/server_expressjs.html
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Content-Type, Accept");
   next();
 });
