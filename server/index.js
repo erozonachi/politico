19723f56634713daf0fd20756a5726b3dbc9ea75
Eneh <erozonachi.james@gmail.com>
2019-03-11 12:56:47 +0100

    chore(doc): create api documentation
    - ensure all api endpoints are documented and tested using swagger
    - ensure the documentation is accessible via the base and /doc paths
    - ensure all get requests to undefined paths are redirect to /doc path
    
    [Delivers: #164533581]

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import Routes from './routes/Routes';
+import * as Constants from './helpers/Constants';
+import path from 'path';
 
 dotenv.config();
 const app = express();
@@ -21,8 +23,16 @@ app.use((req, res, next) => { // https://enable-cors.org/server_expressjs.html
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-access-token, Content-Type, Accept");
   next();
 });
+app.use(express.static(`${__dirname}/doc`));
 
 Routes(app);
+// GET /api/v1/doc
+app.get(`${Constants.API_BASE_URL}/doc`, (req, res) => {
+  res.sendFile(path.join(`${__dirname}/doc/index.html`));
+});
+app.get(`/*`, (req, res) => {
+  res.redirect(`${Constants.API_BASE_URL}/doc`);
+});
 app.listen(port, () => {
   console.log(`Live on ${port}`);
 });
